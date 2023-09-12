0fa1dcc7a8f63ebea6e9fff57674a93ae55e3237
jp <105756115+jpG98@users.noreply.github.com>
2023-09-12 10:05:26 -0300

    Add files via upload

---
+++ imc/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>imc</title>
+</head>
+<body>
+    <form action="resultado.php">
+    <p>imc</p>
+        <label>peso:</label>
+        <input type="textarea" name="peso"><br>
+        <label>altura:</label>
+        <input type="textarea" name="altura"><br>
+
+        <input type="submit">
+    </form>
+</body>
+</html>
