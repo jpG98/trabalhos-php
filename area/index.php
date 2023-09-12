d46e89258fcb2c38fad99bf976081f439e10c4d3
jp <105756115+jpG98@users.noreply.github.com>
2023-09-12 10:23:23 -0300

    Add files via upload

---
+++ area/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>area do triangulo</title>
+</head>
+<body>
+    <form action="resultado.php" method="post">
+        <p>Area do Triangulo</p> 
+         <input type="Textarea" id="Base" name="base"> 
+         <label for="Base">Base</label><br>      
+         <input type="Textarea" id="Altura" name="altura">
+         <label for="Altura">Altura</label><br>
+         <input type="submit">
+    </form>
+</body>
+</html>
---
+++ area/resultado.php
@@ -0,0 +1,8 @@
+<?php
+
+$altura = $_POST["altura"];
+$base = $_POST["base"];
+$resultado = ($base * $altura)/2;
+
+echo $resultado;
+?>
